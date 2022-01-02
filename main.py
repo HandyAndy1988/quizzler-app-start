6f0cf285e2e824daadb602e0e5cd254ca3f047e7
Andrew Monk <50429508+HandyAndy1988@users.noreply.github.com>
2022-01-02 19:25:01 +0900

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
-
+# test
 question_bank = []
 for question in question_data:
     question_text = question["question"]
