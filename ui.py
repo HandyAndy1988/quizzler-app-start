bd113a9b0d5be310eae99085cb7f7dacc942ef33
Andrew Monk <sephiroths_clone@hotmail.com>
2022-01-04 19:27:22 +0900

    Added flashes to show correct/incorrect

--- ui.py
+++ ui.py
@@ -8,7 +8,6 @@ class QuizInterface:
 
     def __init__(self, quiz_brain: QuizBrain):
         self.quiz = quiz_brain
-
         score = 0
         self.window = Tk()
         self.window.title("Quizzler")
@@ -25,15 +24,33 @@ class QuizInterface:
 
         true_image = PhotoImage(file="images/true.png")
         false_image = PhotoImage(file="images/false.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
+
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0, padx=20, pady=20)
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1, padx=20, pady=20)
 
         self.get_next_question()
-
+        # self.give_feedback()
         self.window.mainloop()
 
     def get_next_question(self):
         q_text = self.quiz.next_question()
+        self.canvas.config(bg="white")
         self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
+
